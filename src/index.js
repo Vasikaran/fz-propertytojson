39026ec2ecd111fd47bc3aefd655bce81d48e116
Vimalesan <vimalesan.ceg@gmail.com>
2017-08-22 00:54:04 +0530

    property json - js properties as json object

--- src/index.js
+++ src/index.js
@@ -1,76 +1,107 @@
 var fs = require('fs');
 var path = require('path');
-function isComment(str){
-	return str.startsWith("#")
+function isComment(str) {
+  return str.startsWith('#');
 }
-function stringToJson(str){
-  var lines = str.split("\n");
-  var json={};
-  lines.forEach((line)=>{
-    if(!isComment(line) && !(line == "")){
-      var indexOfEqual=line.indexOf("=");
-      var key =  line.substring(0,indexOfEqual);
-      var value =  line.substring(indexOfEqual+1);
-      if(!json[key]){
-        json[key] =  value;
+function stringToJson(str) {
+  var lines = str.split('\n');
+  var json = {};
+  lines.forEach(line => {
+    if (!isComment(line) && !(line == '')) {
+      var indexOfEqual = line.indexOf('=');
+      var key = line.substring(0, indexOfEqual);
+      var value = line.substring(indexOfEqual + 1);
+      if (!json[key]) {
+        json[key] = value;
       }
     }
-  })
-  return json
+  });
+  return json;
 }
-function jsonToString(json){
-    var str = "var i18n={";
-    var keys = Object.keys(json);
-    keys.forEach((key,i)=>{
-      var value=json[key];
-      str+="\""+key+"\":\""+value.replace(/.?\"/g,function(match){
-              if(match[0]=="\\"){
-                return match;
-              }
-              else if(match.length==2){
-                return match[0]+"\\"+match[1]
-              }
-              else{
-                return "\\"+match
-              }
-            }).replace(/(\r\n|\n|\r)/g,"")+"\"";
-      if(i!=keys.length-1)
-        str+=","
-    })
-    str+="}";
-    return str;
+function jsonToString(json, context) {
+  var str = 'var ' + context.trim() + 'I18n={';
+  var keys = Object.keys(json);
+  keys.forEach((key, i) => {
+    var value = json[key];
+    str +=
+      '"' +
+      key +
+      '":"' +
+      value
+        .replace(/.?\"/g, function(match) {
+          if (match[0] == '\\') {
+            return match;
+          } else if (match.length == 2) {
+            return match[0] + '\\' + match[1];
+          } else {
+            return '\\' + match;
+          }
+        })
+        .replace(/(\r\n|\n|\r)/g, '') +
+      '"';
+    if (i != keys.length - 1) str += ',';
+  });
+  str += '}';
+  return str;
 }
-export default function folderIterate(propertyFolder, i18nFolder, baseProperty, jsProperty){
+export default function folderIterate(
+  propertyFolder,
+  i18nFolder,
+  baseProperty,
+  jsProperty,
+  context = 'i18n'
+) {
   var basePropertyStr = fs.readFileSync(baseProperty).toString();
   var basePropertyJson = stringToJson(basePropertyStr);
-  var jsPropertyStr =  fs.readFileSync(jsProperty).toString();
-  var jsPropertyJson = stringToJson(jsPropertyStr);
+  var jsPropertyStr = fs.readFileSync(jsProperty).toString();
+  try {
+    var jsPropertyJson = JSON.parse(jsPropertyStr);
+  } catch (e) {
+    var jsPropertyJson = stringToJson(jsPropertyStr);
+  }
 
-  fs.readdir(propertyFolder, (err, files) => {
-    files.forEach(file => {
-    	var location=path.join(propertyFolder,file);
-    	if(fs.statSync(location).isDirectory()){
-    		console.log("dir"+file)
-    	}else{
-        if(location!=jsProperty && file.endsWith(".properties")){
-      		var str = fs.readFileSync(location).toString()
-      		var json=stringToJson(str)
-          var outputJson = Object.keys(jsPropertyJson).reduce((result,key)=>{
-            result[key]=json[key] || basePropertyJson[key] || jsPropertyJson[key];
+  fs.readdir(propertyFolder, function(err, files) {
+    files.forEach(function(file) {
+      var location = path.join(propertyFolder, file);
+      if (fs.statSync(location).isDirectory()) {
+        console.log('dir' + file);
+      } else {
+        if (
+          location != jsProperty &&
+          location != baseProperty &&
+          file.endsWith('.properties')
+        ) {
+          var str = fs.readFileSync(location).toString();
+          var json = stringToJson(str);
+          var outputJson = Object.keys(jsPropertyJson).reduce(function(
+            result,
+            key
+          ) {
+            result[key] =
+              json[key] || basePropertyJson[key] || jsPropertyJson[key];
             return result;
-          },{})
-          var underScoreIndex = file.indexOf("_");
-          var outputFileName = "en_US.js";
-          if(underScoreIndex != -1){
-            outputFileName = file.substring(underScoreIndex+1,file.indexOf("."));  
-            outputFileName += ".js";
+          }, {});
+          var underScoreIndex = file.indexOf('_');
+          var outputFileName = 'en_US.js';
+          if (underScoreIndex != -1) {
+            outputFileName = file.substring(
+              underScoreIndex + 1,
+              file.indexOf('.')
+            );
+            outputFileName += '.js';
           }
-          console.log(file)
-          fs.writeFileSync(path.join(i18nFolder,outputFileName),jsonToString(outputJson));
+          console.log(file);
+          if (!fs.existsSync(i18nFolder)) {
+            fs.mkdir(i18nFolder);
+          }
+          fs.writeFileSync(
+            path.join(i18nFolder, outputFileName),
+            jsonToString(outputJson, context)
+          );
         }
-    	}
+      }
     });
-  })
+  });
 }
 
 /*var propertyFolder = process.argv[2];
